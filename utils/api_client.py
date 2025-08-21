4891f8af9cc2c96379dbfd4887053dc2927c229a
FloraBucara <proyectoflorabu@gmail.com>
2025-08-21 16:42:29 -0500

    Update: Mejoras en el modelo ONNX

--- utils/api_client.py
+++ utils/api_client.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 
 # Configuración del servidor
-SERVER_URL = " "  # Cambiar a ngrok cuando lo tengamos
+SERVER_URL = ""  # Cambiar a ngrok cuando lo tengamos
 
 def verificar_servidor():
     """Verifica si el servidor está disponible"""
